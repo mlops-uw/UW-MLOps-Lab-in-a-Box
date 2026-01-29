d1a1492dd284104b7ebf5c4e490500203504b4bf
Archit <archit@Sandeeps-MacBook-Air.local>
2026-01-29 10:30:16 -0800

    Commenting Terraform file

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,27 +1,27 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "=4.1.0"
-    }
-  }
-}
+# terraform {
+#   required_providers {
+#     azurerm = {
+#       source  = "hashicorp/azurerm"
+#       version = "=4.1.0"
+#     }
+#   }
+# }
 
-provider "azurerm" {
-  features {}
-  resource_provider_registrations = "none"
+# provider "azurerm" {
+#   features {}
+#   resource_provider_registrations = "none"
 
-}
+# }
 
-data "azurerm_resource_group" "example" {
-  name     = "mlops"
-}
+# data "azurerm_resource_group" "example" {
+#   name     = "mlops"
+# }
 
-# Create a virtual network within the resource group
-resource "azurerm_virtual_network" "example" {
-  name                = "example-network"
-  resource_group_name = data.azurerm_resource_group.example.name
-  location            = data.azurerm_resource_group.example.location
-  address_space       = ["10.0.0.0/16"]
-}
+# # Create a virtual network within the resource group
+# resource "azurerm_virtual_network" "example" {
+#   name                = "example-network"
+#   resource_group_name = data.azurerm_resource_group.example.name
+#   location            = data.azurerm_resource_group.example.location
+#   address_space       = ["10.0.0.0/16"]
+# }
 
