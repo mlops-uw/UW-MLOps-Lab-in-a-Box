7a933431942d93137bc147a5bc655d42302dbff2
Archit <archit@Sandeeps-MacBook-Air.local>
2026-02-15 06:44:57 -0800

    Update Storage Container Name

--- terraform/main.tf
+++ terraform/main.tf
@@ -26,7 +26,7 @@ resource "azurerm_storage_account" "example" {
 }
 
 resource "azurerm_storage_container" "example" {
-  name                  = "Taxi Data"
+  name                  = "taxidata"
   storage_account_name    = azurerm_storage_account.example.name
   container_access_type = "private"
 }
