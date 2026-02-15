91f0128c3e994dc516bcc324db40b461ebd4c168
Archit <archit@Sandeeps-MacBook-Air.local>
2026-02-15 07:40:55 -0800

    Check Storage Container ID

--- terraform/output.tf
+++ terraform/output.tf
@@ -2,3 +2,8 @@ output "storage_account_id" {
   value       = azurerm_storage_account.example.id
   description = "The ID of the storage account"
 }
+
+output "storage_container_id" {
+  value       = azurerm_storage_container.example.id
+  description = "The ID of the storage container"
+}
