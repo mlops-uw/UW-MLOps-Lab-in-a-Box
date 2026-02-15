e5f025aabed6a2136691c2b0abf79ccd00af54d4
Archit <archit@Sandeeps-MacBook-Air.local>
2026-02-15 07:59:38 -0800

    Create All Resources

--- terraform/main.tf
+++ terraform/main.tf
@@ -43,37 +43,38 @@ resource "azurerm_storage_container" "example" {
 #   source                 = "../taxi_zone_lookup.csv"
 # }
 
-# resource "azurerm_application_insights" "example" {
-#   name                = "workspace-mlops"
-#   location            = data.azurerm_resource_group.example.location
-#   resource_group_name = data.azurerm_resource_group.example.name
-#   application_type    = "web"
-# }
+resource "azurerm_application_insights" "example" {
+  name                = "workspace-mlops"
+  location            = data.azurerm_resource_group.example.location
+  resource_group_name = data.azurerm_resource_group.example.name
+  application_type    = "web"
+}
 
-# resource "azurerm_key_vault" "example" {
-#   name                = "workspace-mlops-keyvault"
-#   location            = data.azurerm_resource_group.example.location
-#   resource_group_name = data.azurerm_resource_group.example.name
-#   tenant_id           = data.azurerm_client_config.current.tenant_id
-#   sku_name            = "premium"
-# }
+resource "azurerm_key_vault" "example" {
+  name                = "workspace-mlops-keyvault"
+  location            = data.azurerm_resource_group.example.location
+  resource_group_name = data.azurerm_resource_group.example.name
+  tenant_id           = data.azurerm_client_config.current.tenant_id
+  sku_name            = "premium"
+}
 
-# resource "azurerm_machine_learning_workspace" "example" {
-#   name                    = "mlops-workspace"
-#   location                = data.azurerm_resource_group.example.location
-#   resource_group_name     = data.azurerm_resource_group.example.name
-#   application_insights_id = azurerm_application_insights.example.id
-#   key_vault_id            = azurerm_key_vault.example.id
-#   storage_account_id      = azurerm_storage_account.example.id
+resource "azurerm_machine_learning_workspace" "example" {
+  name                    = "mlops-workspace"
+  location                = data.azurerm_resource_group.example.location
+  resource_group_name     = data.azurerm_resource_group.example.name
+  application_insights_id = azurerm_application_insights.example.id
+  key_vault_id            = azurerm_key_vault.example.id
+  storage_account_id      = azurerm_storage_account.example.id
 
-#   identity {
-#     type = "SystemAssigned"
-#   }
-# }
+  identity {
+    type = "SystemAssigned"
+  }
+}
 
-# resource "azurerm_machine_learning_datastore_blobstorage" "example" {
-#   name                 = "mlops-datastore"
-#   workspace_id         = azurerm_machine_learning_workspace.example.id
-#   storage_container_id = azurerm_storage_container.example.id
-#   account_key          = azurerm_storage_account.example.primary_access_key
-# }
+
+resource "azurerm_machine_learning_datastore_blobstorage" "example" {
+ name                 = "mlops-datastore"
+ workspace_id         = azurerm_machine_learning_workspace.example.id
+ storage_container_id = "${azurerm_storage_account.example.id}/blobServices/default/containers/${azurerm_storage_container.example.name}"
+ account_key          = azurerm_storage_account.example.primary_access_key
+}
