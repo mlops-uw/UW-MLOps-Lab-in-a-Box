c66467aa8c0903b9cd18c8de6843335640acabc6
Archit <archit@Sandeeps-MacBook-Air.local>
2026-02-15 10:55:47 -0800

    Create All Resources

--- terraform/main.tf
+++ terraform/main.tf
@@ -55,7 +55,7 @@ resource "azurerm_application_insights" "example" {
 }
 
 resource "azurerm_key_vault" "example" {
-  name                = "capstonekeyvault"
+  name                = "capstonemlopsvault"
   location            = data.azurerm_resource_group.example.location
   resource_group_name = data.azurerm_resource_group.example.name
   tenant_id           = data.azurerm_client_config.current.tenant_id
@@ -74,7 +74,6 @@ resource "azurerm_machine_learning_workspace" "example" {
     type = "SystemAssigned"
   }
 }
-
 
 resource "azurerm_machine_learning_datastore_blobstorage" "example" {
  name                 = "mlops_datastore"
