7f915e5703fb3d519a641682414c33e69b61f178
Archit <archit@Sandeeps-MacBook-Air.local>
2026-02-16 09:59:39 -0800

    Check ssh key works

--- terraform/output.tf
+++ terraform/output.tf
@@ -1,12 +1,12 @@
-output "storage_account_id" {
-  value       = azurerm_storage_account.example.id
-  description = "The ID of the storage account"
-}
+# output "storage_account_id" {
+#   value       = azurerm_storage_account.example.id
+#   description = "The ID of the storage account"
+# }
 
-output "storage_container_id" {
-  value       = azurerm_storage_container.example.id
-  description = "The ID of the storage container"
-}
+# output "storage_container_id" {
+#   value       = azurerm_storage_container.example.id
+#   description = "The ID of the storage container"
+# }
 
 output "ssh_public_key"{
   value       = var.ssh_public_key
