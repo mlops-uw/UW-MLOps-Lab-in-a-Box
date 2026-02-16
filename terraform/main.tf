1028029e3633b830d9c997f768de4476fe1b7ca7
Archit <archit@Sandeeps-MacBook-Air.local>
2026-02-16 10:55:33 -0800

    Creating ML Compute Instance with shared authorization

--- terraform/main.tf
+++ terraform/main.tf
@@ -55,7 +55,7 @@ resource "azurerm_application_insights" "example" {
 }
 
 resource "azurerm_key_vault" "example" {
-  name                = "new_capstonevault"
+  name                = "newcapstonevault"
   location            = data.azurerm_resource_group.example.location
   resource_group_name = data.azurerm_resource_group.example.name
   tenant_id           = data.azurerm_client_config.current.tenant_id
@@ -63,7 +63,7 @@ resource "azurerm_key_vault" "example" {
 }
 
 resource "azurerm_machine_learning_workspace" "example" {
-  name                    = "new_workspacecapstone"
+  name                    = "newworkspacecapstone"
   location                = data.azurerm_resource_group.example.location
   resource_group_name     = data.azurerm_resource_group.example.name
   application_insights_id = azurerm_application_insights.example.id
