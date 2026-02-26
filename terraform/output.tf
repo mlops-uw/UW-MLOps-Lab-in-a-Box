5bd191b40229afdf17ac85eb3674672ddebecb4b
Archit <archit@Sandeeps-MacBook-Air.local>
2026-02-26 11:26:45 -0800

    Modularize the structure

--- terraform/output.tf
+++ terraform/output.tf
@@ -1,9 +1,9 @@
 output "storage_account_id" {
-  value       = module.storage.id
+  value       = module.storage_account_id.id
   description = "The ID of the storage account"
 }
 
 output storage_container_name {
-  value       = module.storage.name
+  value       = module.storage_container_name.name
   description = "The name of the storage container"
 }
