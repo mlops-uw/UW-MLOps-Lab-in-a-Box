318bfe12964f09f013b618655c0ea8d8fa624652
Archit <archit@Sandeeps-MacBook-Air.local>
2026-03-02 21:23:28 -0800

    Update secret

--- terraform/storage/main.tf
+++ terraform/storage/main.tf
@@ -4,6 +4,7 @@ resource "azurerm_storage_account" "account" {
   location                 = var.location
   account_tier             = var.account_tier
   account_replication_type = var.account_replication_type
+  shared_access_key_enabled = true
 }
 
 resource "azurerm_storage_container" "container" {
