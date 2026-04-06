0930b328e1e5231a01d7d8399bc2217bef4d47d5
Archit <archit@Sandeeps-Air.hsd1.wa.comcast.net>
2026-04-05 19:54:43 -0700

    Change permission to compute instance

--- terraform/machinelearning/main.tf
+++ terraform/machinelearning/main.tf
@@ -1,30 +1,30 @@
-# resource "azurerm_application_insights" "insights" {
-#   name                = var.application_insights_name
-#   location            = var.location
-#   resource_group_name = var.resource_group_name
-#   application_type    = var.application_type
-# }
+resource "azurerm_application_insights" "insights" {
+  name                = var.application_insights_name
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  application_type    = var.application_type
+}
 
 
-# resource "azurerm_machine_learning_workspace" "mlworkspace" {
-#   name                    = var.workspace_name
-#   location            = var.location
-#   resource_group_name = var.resource_group_name
-#   application_insights_id = azurerm_application_insights.insights.id
-#   key_vault_id            = var.key_vault_id
-#   storage_account_id      = var.storage_account_id
+resource "azurerm_machine_learning_workspace" "mlworkspace" {
+  name                    = var.workspace_name
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  application_insights_id = azurerm_application_insights.insights.id
+  key_vault_id            = var.key_vault_id
+  storage_account_id      = var.storage_account_id
 
-#   identity {
-#      type = var.type
-#    }
-# }
+  identity {
+     type = var.type
+   }
+}
 
-# resource "azurerm_machine_learning_datastore_blobstorage" "blob" {
-#  name                 = var.datastore_name
-#  workspace_id         = azurerm_machine_learning_workspace.mlworkspace.id
-#  storage_container_id = var.storage_container_id
-#  account_key          = var.account_key
-# }
+resource "azurerm_machine_learning_datastore_blobstorage" "blob" {
+ name                 = var.datastore_name
+ workspace_id         = azurerm_machine_learning_workspace.mlworkspace.id
+ storage_container_id = var.storage_container_id
+ account_key          = var.account_key
+}
 
 resource "azurerm_machine_learning_compute_instance" "example" {
   name                          = var.ml_instance_name
--- terraform/main.tf
+++ terraform/main.tf
@@ -25,28 +25,28 @@ data "azurerm_resource_group" "example" {
   name     = var.resource_group_name
 }
 
-# module "storage" {
-#   source = "./storage"
+module "storage" {
+  source = "./storage"
 
-#   name                     = var.name
-#   resource_group_name      = data.azurerm_resource_group.example.name
-#   location                 = data.azurerm_resource_group.example.location
-#   account_tier             = var.account_tier
-#   account_replication_type = var.account_replication_type
-#   container_name           = var.container_name
-#   container_access_type    = var.container_access_type
-# }
+  name                     = var.name
+  resource_group_name      = data.azurerm_resource_group.example.name
+  location                 = data.azurerm_resource_group.example.location
+  account_tier             = var.account_tier
+  account_replication_type = var.account_replication_type
+  container_name           = var.container_name
+  container_access_type    = var.container_access_type
+}
 
-# module "network" {
-#   source = "./network"
+module "network" {
+  source = "./network"
 
-#   virtual_network_name = var.virtual_network_name 
-#   address_space = var.address_space
-#   resource_group_name      = data.azurerm_resource_group.example.name
-#   location                 = data.azurerm_resource_group.example.location
-#   subnet_name = var.subnet_name
-#   address_prefixes = var.address_prefixes
-# }
+  virtual_network_name = var.virtual_network_name 
+  address_space = var.address_space
+  resource_group_name      = data.azurerm_resource_group.example.name
+  location                 = data.azurerm_resource_group.example.location
+  subnet_name = var.subnet_name
+  address_prefixes = var.address_prefixes
+}
 
 module "machinelearning"{
   source = "./machinelearning"
@@ -71,16 +71,16 @@ module "machinelearning"{
   tags = var.tags
 }
 
-# module "keyvault" {
-#   source = "./keyvault"
+module "keyvault" {
+  source = "./keyvault"
 
-#   resource_group_name      = data.azurerm_resource_group.example.name
-#   location                 = data.azurerm_resource_group.example.location
-#   key_vault_name = var.key_vault_name
-#   secret_name = var.secret_name
-#   secret_value = module.storage.primary_connection_string
-#   sku_name = var.sku_name
-#   object_id_sp = data.azurerm_client_config.current.object_id
-#   object_id = var.object_id  
-#   tenant_id =  data.azurerm_client_config.current.tenant_id
-# }
+  resource_group_name      = data.azurerm_resource_group.example.name
+  location                 = data.azurerm_resource_group.example.location
+  key_vault_name = var.key_vault_name
+  secret_name = var.secret_name
+  secret_value = module.storage.primary_connection_string
+  sku_name = var.sku_name
+  object_id_sp = data.azurerm_client_config.current.object_id
+  object_id = var.object_id  
+  tenant_id =  data.azurerm_client_config.current.tenant_id
+}
